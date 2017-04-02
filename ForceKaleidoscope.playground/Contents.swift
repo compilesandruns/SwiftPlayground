fe5dbe7eb677de7571576c48b71ced5905a1e7d9
Stephanie Guevara <stephanie.n.guevara@gmail.com>
2017-04-02 12:49:17 -0400

    wip

--- ForceKaleidoscope.playground/Contents.swift
+++ ForceKaleidoscope.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 import SpriteKit
 
 import PlaygroundSupport
@@ -54,76 +55,84 @@ extension CGPoint {
 
 class GameScene: SKScene {
     
-    let line = SKShapeNode(circleOfRadius: 5.0)
+    var lastPoint: CGPoint!
+    var currentPoint: CGPoint!
+    
+    var kaleidoscope: SKSpriteNode!
+    var temp: SKSpriteNode!
     
     override init(size: CGSize) {
         super.init(size: size)
         
-        backgroundColor = .black
+        backgroundColor = .clear
         
         //Blur effect
-        let effectsNode = SKEffectNode()
-        let filter = CIFilter(name: "CIGaussianBlur")
-        let blurAmount = 10.0
-        filter?.setValue(blurAmount, forKey: kCIInputRadiusKey)
-        addChild(effectsNode)
+//        let effectsNode = SKEffectNode()
+//        let filter = CIFilter(name: "CIGaussianBlur")
+//        let blurAmount = 10.0
+//        filter?.setValue(blurAmount, forKey: kCIInputRadiusKey)
+//        addChild(effectsNode)
         
         //Circle
-        let kaleidoscope = SKSpriteNode(color: .white, size: CGSize(width: size.height * 0.8, height: size.height * 0.8))
+        kaleidoscope = SKSpriteNode(color: .white, size: CGSize(width: size.height * 0.8, height: size.height * 0.8))
         kaleidoscope.position = CGPoint(x: frame.width * 0.5, y:frame.height * 0.5)
         addChild(kaleidoscope)
         
-        
+        //Temp
+        temp = SKSpriteNode(color: .clear, size: CGSize(width: size.height * 0.8, height: size.height * 0.8))
+        temp.position = CGPoint(x: frame.width * 0.5, y:frame.height * 0.5)
+        addChild(temp)
+
+
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func didMove(to view: SKView) {
-        line.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-        line.strokeColor = .purple
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first?.location(in: temp)
+        lastPoint = location
         
-        addChild(line)
-
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
-        }
-        let touchLocation = touch.location(in: self)
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first?.location(in: temp)
+        currentPoint = location
         
-        // 2 - Set up initial location of projectile
-        let forward = SKShapeNode(circleOfRadius: 3)
-        forward.strokeColor = .red
-        forward.position = line.position
+        let pathToDraw = CGMutablePath()
+        let tempLine = SKShapeNode(path:pathToDraw)
         
-        // 3 - Determine offset of location to projectile
-        let offset = touchLocation - forward.position
-    
+        temp.removeAllChildren()
         
-        // 5 - OK to add now - you've double checked position
-        addChild(forward)
+        pathToDraw.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        pathToDraw.addLine(to: CGPoint(x: currentPoint.x, y: currentPoint.y))
         
-        // 6 - Get the direction of where to shoot
-        let direction = offset.normalized()
+        tempLine.path = pathToDraw
+        tempLine.strokeColor = .green
         
-        // 7 - Make it shoot far enough to be guaranteed off screen
-        let shootAmount = direction * 1000
-        
-        // 8 - Add the shoot amount to the current position
-        let realDest = shootAmount + forward.position
-        
-        // 9 - Create the actions
-        let actionMove = SKAction.move(to: realDest, duration: 2.0)
-        let actionMoveDone = SKAction.removeFromParent()
-        forward.run(SKAction.sequence([actionMove, actionMoveDone]))
+        temp.addChild(tempLine)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        let location = touches.first?.location(in: temp)
+        currentPoint = location
+        
+        let pathToDraw = CGMutablePath()
+        let myLine = SKShapeNode(path:pathToDraw)
+        
+        pathToDraw.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        pathToDraw.addLine(to: CGPoint(x: currentPoint.x, y: currentPoint.y))
+        
+        myLine.path = pathToDraw
+        myLine.strokeColor = .blue
+        
+        kaleidoscope.addChild(myLine)
+        
+        currentPoint = nil
+        lastPoint = nil
     }
+    
 }
 
 let vc = GameViewController()
